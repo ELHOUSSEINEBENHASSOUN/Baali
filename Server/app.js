93a58f8ab95859f7d277f5e229a1fd2b65a39aef
Minajrhaidar <amina.jrhaidar@gmail.com>
2023-12-23 23:40:52 +0100

    Ajout du modèle de notification

--- Server/app.js
+++ Server/app.js
@@ -1,23 +1,37 @@
 const express = require('express');
-require('./config/database');
-const config = require('config');
+const morgan= require('morgan');
+const joi = require('joi');
 const helmet = require('helmet');
-const morgan = require('morgan');
 const dotenv = require('dotenv');
-const Joi = require('joi');
+require('./config/database');
+
+
+
+
+dotenv.config({path: '.env'});
+
+
+
 const app = express();
 
-
 // Middlewares de ExpressJS
-app.use(express.json()); // send data with JSON
 app.use(express.urlencoded()); // send data with Form URLencoded
-app.use(express.static('public')); // Access to the file static
+app.use(express.json()); // send data with JSON
+// app.use(express.static('public')); // Access to the file static
 app.use(helmet()); // To Secure requestes HTTP
 app.use(morgan('tiny')); // Status of the requestes HTTP and 'tiny' responsable for format msg
-dotenv.config({ path: '.env' }) // Get value PORT from file .env
 
 
-// export SUPPRESS_NO_CONFIG_WARNING = 1
-// Pour créer un port environment Tapez la commande : " export PORT=5000 "
-const port = process.env.PORT || 4000;
-app.listen(port, () => console.log(`Listenning to the port ${port} ...`));
+//Routes
+// app.use('/api/v1/account', accountRoute)+const accountRoute = require('./routes/accountRoute');
+
+// app.use('/api/v1/account', require('./routes/accountRoute'));
+app.use('/api/v1/notification', require('./routes/notificationRoute'));
+// app.use('/api/v1/transaction', require('./routes/transactionRoute'));
+
+
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
---
+++ Server/routes/notificationRoute.js
@@ -0,0 +1,14 @@
+const express= require('express');
+const router= express.Router();
+
+const {createNotification, getAllNotifications, getNotificationById, updateNotificationById, deleteNotificationById}= require('../controllers/notificationController');
+
+//routes
+router.post('/addNotif', createNotification);
+router.get('/getNotif/:id', getNotificationById);
+router.get('/getNotif', getAllNotifications);
+router.put('/upNotif/:id', updateNotificationById);
+router.delete('/delNotif/:id', deleteNotificationById);
+
+
+module.exports = router;
