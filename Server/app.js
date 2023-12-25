9f186452c0256a8884156cd1c977be36955b7dab
Mounir BAALI <mounir.baali@outlook.fr>
2023-12-25 13:53:38 +0100

    v1.02

--- Server/app.js
+++ Server/app.js
@@ -28,6 +28,7 @@ app.use(morgan('tiny')); // Status of the requestes HTTP and 'tiny' responsable 
 // app.use('/api/v1/account', require('./routes/accountRoute'));
 app.use('/api/v1/account', require('./routes/accountRoute'));
 app.use('/api/v1/category', require('./routes/categoryRoute'));
+app.use('/api/v1/offer', require('./routes/offerRoute'));
 app.use('/api/v1/notification', require('./routes/notificationRoute'));
 // app.use('/api/v1/transaction', require('./routes/transactionRoute'));
 
